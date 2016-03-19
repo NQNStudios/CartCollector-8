b2387f8cdb07e63a976c3613bdcc85bb9dfa71a0
Nathaniel Nelson <nelson.nleroy@gmail.com>
2016-03-19 10:13:10 -0600

    Preparing to allow paginated selection of carts

--- cart-launcher/CartLauncher.cpp
+++ cart-launcher/CartLauncher.cpp
@@ -1,19 +1,45 @@
+#include <cstdlib>
+#include <math.h>
+
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
+
+#include <dirent.h>
 
 #include "SDL.h"
 #include "SDL_ttf.h"
 #include "SDL_image.h"
 
 
+const int CARTS_PER_PAGE = 10;
+
 struct State
 {
-    bool Running = true;
+    bool Running=true;
+    string CartDirectory;
+    vector<string> Carts;
+    int Page=0;
+    int MaxPage;
+    int SelectedIndex=0;
+    SDL_Texture* CartImage=NULL;
 };
 
-void Init(State* state);
-void Update(State* state);
-void Draw(SDL_Renderer* renderer, TTF_Font* font);
+void Init();
+void Update();
+void Draw();
+
+int SelectedCart();
+void SetSelectedIndex(int index);
+void SetPage(int page);
+void LoadCartImage();
+
+State* state;
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+TTF_Font* font;
 
 int main(int argc, char** argv)
 {
@@ -22,7 +48,7 @@ int main(int argc, char** argv)
     TTF_Init();
 
     // Open a TODO fullscreen window
-    SDL_Window* window = SDL_CreateWindow(
+    window = SDL_CreateWindow(
             "CartLauncher-8",
             SDL_WINDOWPOS_UNDEFINED,
             SDL_WINDOWPOS_UNDEFINED,
@@ -33,27 +59,24 @@ int main(int argc, char** argv)
     if (window)
     {
         // Create a renderer
-        SDL_Renderer* renderer = SDL_CreateRenderer(
-                window, -1, SDL_RENDERER_ACCELERATED);
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
         // Make sure the renderer was created properly
         if (renderer)
         {
             // Get a font for text
-            TTF_Font* font = TTF_OpenFont("font.ttf", 18);
+            font = TTF_OpenFont("font.ttf", 18);
 
             // Make sure font was opened properly
             if (font)
             {
-                // Initialize the launcher state
-                State state;
-                Init(&state);
+                Init();
 
                 // Start the functioning loop
-                while(state.Running)
+                while(state->Running)
                 {
-                    Update(&state);
-                    Draw(renderer, font);
+                    Update();
+                    Draw();
                     SDL_Delay(16);
                 }
             }
@@ -86,11 +109,38 @@ int main(int argc, char** argv)
 }
 
 SDL_Texture* testCart;
-void Init(State* state)
+void Init()
 {
+    // Retrieve the cart directory
+    state->CartDirectory = getenv("CART_COLLECTOR_DIR");
+
+    // Extract a list of every cart installed
+    DIR* dir;
+    struct dirent *ent;
+    dir = opendir(state->CartDirectory.c_str());
+
+    if (dir != NULL)
+    {
+        while ((ent = readdir(dir)) != NULL)
+        {
+            state->Carts.push_back(ent->d_name);
+            cout << ent->d_name << endl;
+        }
+
+        closedir(dir);
+    }
+    else
+    {
+        cout << "Error! Failed to open cart directory: "
+            << state->CartDirectory << endl;
+    }
+
+    // Calculate how many pages we will have
+    int carts = state->Carts.size();
+    state->MaxPage = ceil((float) carts / (float) CARTS_PER_PAGE);
 }
 
-void Update(State* state)
+void Update()
 {
     // Process events
     SDL_Event event;
@@ -106,7 +156,7 @@ void Update(State* state)
     }
 }
 
-void Draw(SDL_Renderer* renderer, TTF_Font* font)
+void Draw()
 {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderFillRect(renderer, NULL);
@@ -124,4 +174,51 @@ void Draw(SDL_Renderer* renderer, TTF_Font* font)
     cartDisplayRect.h = 205;
     SDL_RenderCopy(renderer, testCart, NULL, &cartDisplayRect);
     SDL_RenderPresent(renderer);
+}
+
+int SelectedCart()
+{
+    return state->Page * CARTS_PER_PAGE + state->SelectedIndex;
+}
+
+void SetSelectedIndex(int index)
+{
+    // Calculate how many carts are displayed on this page
+    int cartsOnPage = CARTS_PER_PAGE;
+
+    if (state->Page == state->MaxPage)
+    {
+        cartsOnPage = state->Carts.size() % CARTS_PER_PAGE;
+    }
+
+    // Wrap selection between top and bottom
+    if (index < 0) index = cartsOnPage - 1;
+    if (index >= cartsOnPage) index = 0;
+
+    // Change the selected index
+    state->SelectedIndex = index;
+
+    // Load the proper cart image
+    LoadCartImage();
+}
+
+void SetPage(int page)
+{
+    state->Page = page;
+    SetSelectedIndex(0);
+}
+
+void LoadCartImage()
+{
+    if (state->CartImage)
+    {
+        // Free the previously loaded cart image
+        SDL_DestroyTexture(state->CartImage);
+    }
+
+    // Load the new cart image
+    string cartName = state->Carts[SelectedCart()];
+    string imagePath = state->CartDirectory + "/" + cartName;
+    state->CartImage = SDL_CreateTextureFromSurface(
+            renderer, IMG_Load(imagePath.c_str()));
 }
