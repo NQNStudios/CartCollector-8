027580df713af06a095c56d89665b5bd781d1971
Nathaniel Nelson <nelson.nleroy@gmail.com>
2016-03-19 19:06:24 -0600

    Launcher is now functional with keyboard input

--- cart-launcher/CartLauncher.cpp
+++ cart-launcher/CartLauncher.cpp
@@ -1,10 +1,12 @@
 #include <cstdlib>
 #include <math.h>
+#include <stdio.h>
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 using namespace std;
 
 #include <dirent.h>
@@ -14,14 +16,15 @@ using namespace std;
 #include "SDL_image.h"
 
 
-const int CARTS_PER_PAGE = 10;
+const int CARTS_PER_PAGE = 15;
+const string EXTENSION(".p8.png");
 
 struct State
 {
     bool Running=true;
     string CartDirectory;
     vector<string> Carts;
-    int Page=0;
+    int Page=1;
     int MaxPage;
     int SelectedIndex=0;
     SDL_Texture* CartImage=NULL;
@@ -35,6 +38,7 @@ int SelectedCart();
 void SetSelectedIndex(int index);
 void SetPage(int page);
 void LoadCartImage();
+void LaunchCart();
 
 State* state;
 
@@ -42,11 +46,17 @@ SDL_Window* window;
 SDL_Renderer* renderer;
 TTF_Font* font;
 
+SDL_Joystick* joystick;
+
 int main(int argc, char** argv)
 {
     // Open the libraries we need
     SDL_Init(SDL_INIT_EVERYTHING);
     TTF_Init();
+
+    // Prepare for joystick events
+    SDL_JoystickEventState(SDL_ENABLE);
+    joystick = SDL_JoystickOpen(0);
 
     // Open a TODO fullscreen window
     window = SDL_CreateWindow(
@@ -139,8 +149,7 @@ void Init()
         while ((ent = readdir(dir)) != NULL)
         {
             string filename(ent->d_name);
-            string extension(".p8.png");
-            if (filename.find(extension) != string::npos)
+            if (filename.find(EXTENSION) != string::npos)
             {
                 state->Carts.push_back(ent->d_name);
                 cout << ent->d_name << endl;
@@ -174,6 +183,28 @@ void Update()
             case SDL_QUIT:
                 state->Running = false;
                 return;
+
+            case SDL_KEYDOWN:
+                SDL_Keycode key = event.key.keysym.sym;
+                switch (key)
+                {
+                    case SDLK_LEFT:
+                        SetPage(state->Page - 1);
+                        break;
+                    case SDLK_RIGHT:
+                        SetPage(state->Page + 1);
+                        break;
+                    case SDLK_UP:
+                        SetSelectedIndex(state->SelectedIndex-1);
+                        break;
+                    case SDLK_DOWN:
+                        SetSelectedIndex(state->SelectedIndex+1);
+                        break;
+                    case SDLK_RETURN:
+                        LaunchCart();
+                        break;
+                }
+                break;
         }
     }
 }
@@ -182,17 +213,83 @@ void Draw()
 {
     // Clear the screen to black
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderFillRect(renderer, NULL);
+    SDL_RenderClear(renderer);
 
     // Draw the cover of the currently selected cartridge
-    //SDL_Rect cartDisplayRect;
-    //cartDisplayRect.x = 0;
-    //cartDisplayRect.y = 0;
-    //cartDisplayRect.w = 160;
-    //cartDisplayRect.h = 205;
-    //SDL_RenderCopy(renderer, state->CartImage, NULL, &cartDisplayRect);
+    SDL_Rect cartDisplayRect;
+    cartDisplayRect.w = 160*2;
+    cartDisplayRect.h = 205*2;
+    cartDisplayRect.x = 50;
+    cartDisplayRect.y = 480/2-cartDisplayRect.h/2;
+    SDL_RenderCopy(renderer, state->CartImage, NULL, &cartDisplayRect);
+
+    // Define colors for UI text
+    SDL_Color selectedColor;
+    selectedColor.r = 255;
+    selectedColor.g = 255;
+    selectedColor.b = 0;
+    selectedColor.a = 255;
+
+    SDL_Color normalColor;
+    normalColor.r = 255;
+    normalColor.g = 255;
+    normalColor.b = 255;
+    normalColor.a = 255;
+
+    // Draw the current page number
+    int x = 460;
+    int y = cartDisplayRect.y;
+
+    stringstream pageInfo;
+    pageInfo << state->Page << "/" << state->MaxPage;
+    const char* pageInfoStr = pageInfo.str().c_str();
+
+    SDL_Texture* pageInfoTexture = SDL_CreateTextureFromSurface(
+            renderer,
+            TTF_RenderText_Solid(font, pageInfoStr, normalColor));
+
+    SDL_Rect pageInfoRect;
+    pageInfoRect.x = x;
+    pageInfoRect.y = y;
+    TTF_SizeText(font, pageInfoStr, &pageInfoRect.w, &pageInfoRect.h);
+    SDL_RenderCopy(renderer, pageInfoTexture, NULL, &pageInfoRect);
+
+    SDL_DestroyTexture(pageInfoTexture);
 
     // Draw the list of cartridge titles
+    int firstCartOnPage = (state->Page-1) * CARTS_PER_PAGE;
+
+    for (int i = 0; i < CARTS_PER_PAGE; ++i)
+    {
+        // Strip the .p8.png extension from the cart filname
+        string cartTitle = state->Carts[i+firstCartOnPage];
+        cartTitle = cartTitle.substr(0, cartTitle.size() - EXTENSION.size());
+
+        // Display in yellow if the cart is selected
+        SDL_Color color = normalColor;
+        if (state->SelectedIndex == i)
+        {
+            color = selectedColor;
+        }
+
+        // Draw the cart title
+        SDL_Texture* titleTexture = SDL_CreateTextureFromSurface(
+                renderer,
+                TTF_RenderText_Solid(
+                    font,
+                    cartTitle.c_str(),
+                    color));
+
+        y = cartDisplayRect.y + 24 * (i+2);
+
+        SDL_Rect destRect;
+        destRect.x = x;
+        destRect.y = y;
+        TTF_SizeText(font, cartTitle.c_str(), &destRect.w, &destRect.h);
+
+        SDL_RenderCopy(renderer, titleTexture, NULL, &destRect);
+        SDL_DestroyTexture(titleTexture);
+    }
 
     // Show the scene
     SDL_RenderPresent(renderer);
@@ -200,7 +297,7 @@ void Draw()
 
 int SelectedCart()
 {
-    return state->Page * CARTS_PER_PAGE + state->SelectedIndex;
+    return (state->Page-1) * CARTS_PER_PAGE + state->SelectedIndex;
 }
 
 void SetSelectedIndex(int index)
@@ -226,6 +323,9 @@ void SetSelectedIndex(int index)
 
 void SetPage(int page)
 {
+    // Wrap page between 1 and max
+    if (page <= 0) page = state->MaxPage;
+    else if (page > state->MaxPage) page = 1;
     state->Page = page;
     SetSelectedIndex(0);
 }
@@ -243,4 +343,17 @@ void LoadCartImage()
     string imagePath = state->CartDirectory + "/" + cartName;
     state->CartImage = SDL_CreateTextureFromSurface(
             renderer, IMG_Load(imagePath.c_str()));
+}
+
+void LaunchCart()
+{
+    // Get the full path of the cart
+    string cartName = state->Carts[SelectedCart()];
+    string cartPath = state->CartDirectory + "/" + cartName;
+
+    // Launch the cart through PICO-8
+    string launchCommand = "pico8 -run \"" + cartPath + "\"";
+    cout << launchCommand << endl;
+    FILE* in = popen(launchCommand.c_str(), "r");
+    pclose(in);
 }
